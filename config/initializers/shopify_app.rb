7464b6417c1df29a38c913d85eb68eaf7991375c
YooTanya <yootanya@gmail.com>
2016-01-07 12:49:17 +0700

    update api key and secret key

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,6 @@
 ShopifyApp.configure do |config|
-  config.api_key = "97af3c724173d91b10d90f2ddcc428c6"
-  config.secret = "6d702bcc0f1201d3b4c1cd90a4c2978a"
+  config.api_key = "e480c117278884d837e81db5a5707e2d"
+  config.secret = "74873c75f226b74e3366b2a425d63082"
   config.redirect_uri = "http://appbigbuffalo.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
