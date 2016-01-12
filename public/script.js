33f25f12bd264c61a97565a0fd68d4664019f622
YooTanya <yootanya@gmail.com>
2016-01-12 16:24:31 +0700

    added get request

--- public/script.js
+++ public/script.js
@@ -1,1 +1,13 @@
 $(".section-header__title").append( "<p class='test' style='background-color:red;'>Test</p>" );
+
+ $.ajax({
+    type: "GET",
+    url: "https://eazymo-test.myshopify.com/admin/script_tags/count.json",
+    data: { shop: "https://eazymo-test.myshopify.com" },
+    success : function(data) {
+        location.reload();
+    },
+    error : function(error) {
+
+    }
+});
