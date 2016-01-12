fe384b5502818adf68fc9795d170eb925bffb515
YooTanya <yootanya@gmail.com>
2016-01-12 12:42:19 +0700

    removed duplicated code

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,11 +4,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     ShopifyApp.configuration.secret,
     redirect_uri: ShopifyApp.configuration.redirect_uri,
     scope: ShopifyApp.configuration.scope
-  provider :shopify,
-    ShopifyApp.configuration.api_key,
-    ShopifyApp.configuration.secret,
-
-    :redirect_uri => ShopifyApp.configuration.redirect_uri,
-
-    :scope => ShopifyApp.configuration.scope
 end
