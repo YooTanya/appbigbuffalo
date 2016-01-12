f972d7b4bb3ebb10958aaa01601b52e47c4716c7
YooTanya <yootanya@gmail.com>
2016-01-12 13:50:04 +0700

    removed log

--- public/script.js
+++ public/script.js
@@ -1,3 +1,1 @@
-console.log('Hello World');
-console.log('This is second line');
-$(".section-header__title").append( "<p class='test' style='background-colot:red;'>Test</p>" );
+$(".section-header__title").append( "<p class='test' style='background-color:red;'>Test</p>" );
